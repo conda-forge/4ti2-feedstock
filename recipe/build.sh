3548a7082b86cb91f882a63c254d13330699e409
cxzhong <chenxin.zhong@outlook.com>
2026-02-19 13:53:11 +0800

    Fix build script to conditionally run tests during cross-compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+export CFLAGS="${CFLAGS} -D_GNU_SOURCE"
+
 ./configure --prefix=$PREFIX --enable-shared --disable-static
 make -j${CPU_COUNT}
-make check -j${CPU_COUNT}
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
+  make check -j${CPU_COUNT}
+fi
 make install
